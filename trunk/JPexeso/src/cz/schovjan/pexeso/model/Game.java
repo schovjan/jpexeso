20b720264bbab975d8b514506ff61a2f00d83758
schovjan@gmail.com <schovjan@gmail.com@0d6ea3fc-73a9-3c06-e1e4-2519dc841d49>
2014-08-11 11:58:38 +0000

    uprava do modelu, uprava gui

---
+++ trunk/JPexeso/src/cz/schovjan/pexeso/model/Game.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.schovjan.pexeso.model;
+
+/**
+ *
+ * @author schovjan
+ */
+public class Game {
+
+    private Player player1;
+    private Player player2;
+    private int playerOnMove;
+    private int countOfMove;
+
+    public Game() {
+        playerOnMove = 0;
+        countOfMove = 0;
+        player1 = new Player();
+        player2 = new Player();
+    }
+
+    public int getPlayerOnMove() {
+        return playerOnMove;
+    }
+
+    public void setPlayerOnMove(int playerOnMove) {
+        this.playerOnMove = playerOnMove;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    public int getCountOfMove() {
+        return countOfMove;
+    }
+
+    public void setCountOfMove(int countOfMove) {
+        this.countOfMove = countOfMove;
+    }
+
+    public int moveIncrement() {
+        return ++this.countOfMove;
+    }
+
+    public int playerOnMoveIncrement() {
+        return ++this.playerOnMove;
+    }
+
+    public int getTurnedPairsCount() {
+        return player1.getScore() + player2.getScore();
+    }
+}
