a8f4a25ee997c3b562c1854249e3202c25009a3c
schovjan <schovjan@gmail.com>
2014-08-15 07:46:13 +0000

    cistka komentaru v tridach, zavedeni souboru pro konstanty

---
+++ JPexeso/src/cz/schovjan/pexeso/util/Constant.java
@@ -0,0 +1,16 @@
+package cz.schovjan.pexeso.util;
+
+import java.awt.Font;
+
+/**
+ *
+ * @author schovjan
+ */
+public class Constant {
+
+    public static final String APP_NAME = "Pexeso";
+    public static final Font FONT_BIG = new Font(Font.SANS_SERIF, Font.BOLD, 18);
+    public static final Font FONT_SMALL = new Font(Font.SANS_SERIF, Font.PLAIN, 14);
+    public static final String PATH_ICON = "/resource/ico32.png";
+    public static final String PATH_INFORMATION = "resource/information.html";
+}
